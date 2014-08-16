42817b2a29d098dd950d5d0d6b24381dfd8f6c2a
inolve <inolve@informatic.(none)>
2014-08-17 00:16:52 +0200

    prova text

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 $language = "es_ES";
 putenv("LC_ALL=$language");
 setlocale(LC_ALL, $language);
-bindtextdomain("principal", "./locale");
+bindtextdomain("principal", "/var/www/html/locale");
 bind_textdomain_codeset("principal", 'UTF-8');
 textdomain("principal");
 
