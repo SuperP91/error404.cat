11da2958b4b0c92461545fe361752027f3ce32cb
inolve <inolve@informatic.(none)>
2014-08-17 00:27:50 +0200

    prova text

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
-$language = "es_ES";
+$language = "en_US";
 putenv("LC_ALL=$language");
 setlocale(LC_ALL, $language);
 bindtextdomain("principal", "/var/www/html/locale");
