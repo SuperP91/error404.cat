4bd398c08a2fd8587b5ecb3fbbaf4260aad21570
inolve <inolve@informatic.(none)>
2014-08-16 21:27:00 +0200

    test gettetxt

--- test.php
+++ test.php
@@ -3,6 +3,7 @@ $language = "es_ES";
 putenv("LC_ALL=$language");
 setlocale(LC_ALL, $language);
 bindtextdomain("principal", "./locale");
+bind_textdomain_codeset("principal", 'UTF-8');
 textdomain("principal");
 
 print "<p>" . _("Simple test") . "</p>\n";
