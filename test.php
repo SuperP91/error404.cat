f4ded0b50ef747fb32f4266a23e51a90450dc7bc
inolve <inolve@informatic.(none)>
2014-08-17 00:36:02 +0200

    prova text

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 $language = "en_US";
 putenv("LC_ALL=$language");
-setlocale(LC_ALL, $language);
+setlocale(LC_ALL, $language.".utf8");
 bindtextdomain("principal", "/var/www/html/locale");
 bind_textdomain_codeset("principal", 'UTF-8');
 textdomain("principal");
